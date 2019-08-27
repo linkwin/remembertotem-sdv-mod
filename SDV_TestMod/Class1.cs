f1a35853ab7012264bc39583625f7cc926cb2306
linkwin <alan@allteksystemsdesign.com>
2019-08-26 22:56:12 -0400

    Add project files.

---
+++ SDV_TestMod/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDV_TestMod
+{
+    public class Class1
+    {
+    }
+}
